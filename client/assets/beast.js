b01c93e6c44a5ee54bc0199c525791f6b6f65aef
fsaiz97 <felixsaiz97@gmail.com>
2022-09-26 18:01:54 +0100

    Start creating beast page

--- client/assets/beast.js
+++ client/assets/beast.js
@@ -1,8 +1,29 @@
-function getBeastData() {
+async function getBeastData() {
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
 
-    console.log(id)
+    const response = await fetch(`http://localhost:3000/beasts/${id}`);
+    const beast = await response.json();
+
+    return beast;
 }
 
-getBeastData();
+async function displayBeastData() {
+    const beast = await getBeastData();
+    //console.log(beast);
+
+    // Add name
+    const name = document.querySelector("#name");
+    name.textContent = beast.name;
+
+    // Add image
+    const
+
+    // Add habitat
+
+    // Add danger rating
+
+    // Add description
+}
+
+displayBeastData();
