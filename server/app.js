7550d8239135bd19383768220bda1ff2f5ade365
fsaiz97 <felixsaiz97@gmail.com>
2022-09-26 10:39:06 +0100

    Beast data more complex

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 // default server app filename
 
-express = require("express");
+const express = require("express");
+// "Kelpie", "Minotaur", "Hydra", "Hippogriff"
+// { name: "", habitat: "", dangerRating: "", description: "", encounterRate: "", loot: [] }
+let beasts = [
+    { name: "Cyclops", habitat: "Greek  islands", dangerRating: 3, description: "A one-eyed giant", encounterRate: 0.05, loot: ["Cyclops eye", "sheep", "blood"] },
+    { name: "Siren", habitat: "Greek islands", dangerRating: 5, description: "Really good looking, but slightly psychotic", encounterRate: 0.5, loot: ["haunting melody", "sailors' bones", "tail"] },
+    { name: "Minotaur", habitat: "Greek labyrinths", dangerRating: 4, description: "Half-man, half-bull", encounterRate: 0.3, loot: ["six pack", "horns", "thread"] }
+];
 
 // Make a basic server
 app = express();
@@ -11,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/beasts", (req, res) => {
-    res.send(["Kelpie", "Minotaur", "Hydra", "Hippogriff"]);
+    res.send(beasts);
 })
 
 module.exports = app;
