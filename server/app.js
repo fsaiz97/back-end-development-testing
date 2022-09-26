8580f17201b3bc54aaa369f313d3759be4ff21fb
fsaiz97 <felixsaiz97@gmail.com>
2022-09-26 10:42:49 +0100

    Add Hippogriff to data

--- server/app.js
+++ server/app.js
@@ -1,13 +1,8 @@
 // default server app filename
 
 const express = require("express");
-// "Kelpie", "Minotaur", "Hydra", "Hippogriff"
-// { name: "", habitat: "", dangerRating: "", description: "", encounterRate: "", loot: [] }
-let beasts = [
-    { name: "Cyclops", habitat: "Greek  islands", dangerRating: 3, description: "A one-eyed giant", encounterRate: 0.05, loot: ["Cyclops eye", "sheep", "blood"] },
-    { name: "Siren", habitat: "Greek islands", dangerRating: 5, description: "Really good looking, but slightly psychotic", encounterRate: 0.5, loot: ["haunting melody", "sailors' bones", "tail"] },
-    { name: "Minotaur", habitat: "Greek labyrinths", dangerRating: 4, description: "Half-man, half-bull", encounterRate: 0.3, loot: ["six pack", "horns", "thread"] }
-];
+
+const beasts = require("./beasts");
 
 // Make a basic server
 app = express();
---
+++ server/beasts.js
@@ -0,0 +1,12 @@
+// "Kelpie", "Minotaur", "Hydra", "Hippogriff"
+// { name: "", habitat: "", dangerRating: "", description: "", encounterRate: "", loot: [] }
+
+let beasts = [
+    { name: "Cyclops", habitat: "Greek  islands", dangerRating: 3, description: "A one-eyed giant", encounterRate: 0.05, loot: ["Cyclops eye", "sheep", "blood"] },
+    { name: "Siren", habitat: "Greek islands", dangerRating: 5, description: "Really good looking, but slightly psychotic", encounterRate: 0.5, loot: ["haunting melody", "sailors' bones", "tail"] },
+    { name: "Minotaur", habitat: "Greek labyrinths", dangerRating: 4, description: "Half-man, half-bull", encounterRate: 0.3, loot: ["six pack", "horns", "thread"] },
+    { name: "Hippogriff", habitat: "Forbidden Forest", dangerRating: 3, description: "A proud, noble creature. Treat it with respect if you value your life", encounterRate: 0.2, loot: ["Hippogriff's claw", "Wing"] }
+
+];
+
+module.exports = beasts;
